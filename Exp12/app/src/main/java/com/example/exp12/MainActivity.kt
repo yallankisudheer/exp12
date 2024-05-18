6cc071f44d8017b98403167a76733a910176842f
YALLANKI.SUDHEER <211701061@rajalakshmi.edu.in>
2024-05-18 19:06:30 +0530

    Normalize all line endings

---
+++ Exp12/app/src/main/java/com/example/exp12/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.exp12
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    // Define the button and imageview type variable
+    private lateinit var cameraOpenId: Button
+    lateinit var clickImageId: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
+        cameraOpenId = findViewById(R.id.camera_button)
+        clickImageId = findViewById(R.id.click_image)
+
+        // Camera_open button is for open the camera and add the setOnClickListener in this button
+        cameraOpenId.setOnClickListener(View.OnClickListener { v: View? ->
+            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            // Start the activity with camera_intent, and request pic id
+            startActivityForResult(cameraIntent, pic_id)
+        })
+    }
+
+    // This method will help to retrieve the image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+            // BitMap is data structure of image file which store the image in memory
+            val photo = data!!.extras!!["data"] as Bitmap?
+            // Set the image in imageview for display
+            clickImageId.setImageBitmap(photo)
+        }
+    }
+
+    companion object {
+        // Define the pic id
+        private const val pic_id = 123
+    }
+}
